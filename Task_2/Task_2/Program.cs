644e7a22718539e5592009538e015855f4076084
Smoktii <Smoktii@gmail.com>
2020-09-14 11:17:49 +0300

    Task 2 - init

---
+++ Task_2/Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Task 2*/
+            // Task 2
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

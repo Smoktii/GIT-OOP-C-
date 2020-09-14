aad8308c1421ba3c56373359ce71963a7736180b
Smoktii <Smoktii@gmail.com>
2020-09-14 12:13:37 +0300

    task 3

---
+++ Task_3/Task_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            double b;
+            long c;
+            string s;
+            Console.Clear();
+            s = Console.ReadLine();
+            a = Int32.Parse(s);
+            s = Console.ReadLine();
+            b = double.Parse(s);
+            c = long.Parse(s);
+            Console.WriteLine($"a = {a}, b = {b}");
+        }
+    }
+}

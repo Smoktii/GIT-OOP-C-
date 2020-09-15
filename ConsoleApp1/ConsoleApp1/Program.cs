67da4c1247d2d7fd07d475ee2c2909049e6a4156
Smoktii <Smoktii@gmail.com>
2020-09-15 14:07:54 +0300

    Comment

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿// using System;
+
+namespace ConsoleApp1
+{
+    class Program_1
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Привіт, ДонНУ!!!!!!!");
+            int c = 5; int v = 7;
+
+        }
+    }
+
+    class Program_2
+    {
+        static void Main_1(string[] args)
+        {
+            System.Console.WriteLine("Привіт, ДонНУ_22!");
+            int c = 5; int v = 7;
+
+        }
+    }
+}

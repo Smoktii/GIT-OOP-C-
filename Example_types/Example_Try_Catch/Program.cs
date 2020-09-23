cac07426cac399025311c05c437739836f9aed2a
Smoktii <Smoktii@gmail.com>
2020-09-24 00:40:23 +0300

    C1

---
+++ Example_types/Example_Try_Catch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Example_Try_Catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x = 5;
+            int y = x / 0;
+            Console.WriteLine($"Результат: {y}");
+            Console.WriteLine("Конец программы");
+            Console.Read();
+            */ 
+            
+            try
+            {
+                int x = 5;
+                int y = x / 0;
+                Console.WriteLine($"Результат: {y}");
+            }
+            catch
+            {
+                Console.WriteLine("Возникло исключение!");
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally");
+            }
+            Console.WriteLine("Конец программы");
+            Console.Read();
+             
+        }
+    }
+}
---
+++ Example_types/Example_Try_Catch_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Example_Try_Catch_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

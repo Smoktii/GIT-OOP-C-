bcc28259840c73efc0956f1f681a453e800169ba
Smoktii <Smoktii@gmail.com>
2020-09-24 02:06:31 +0300

    С3

--- Example_types/Example_Try_Catch_3/Program.cs
+++ Example_types/Example_Try_Catch_3/Program.cs
@@ -6,6 +6,8 @@ namespace Example_Try_Catch_3
     {
         static void Main(string[] args)
         {
+            System.Console.OutputEncoding = System.Text.Encoding.Unicode;
+            System.Console.InputEncoding = System.Text.Encoding.Unicode;
             // 1 приклад try без finally та catch без параметрів
             try
             {
@@ -41,6 +43,69 @@ namespace Example_Try_Catch_3
             {
                 Console.WriteLine($"3: Виник виняток {ex.Message}");
             }
+
+            // 4 повторний виклик винятка
+            try
+            {
+                int x = 5;
+                int y = x / 0;
+                Console.WriteLine($"Результат: {y}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"4: Виник виняток {ex.Message} передаємо його далі в опрацювання");
+                // throw;
+            }
+
+            // 5 сгенеруємо виняток DivideByZeroException
+            try
+            {
+                int x = 5;
+                int y = x / 1;
+                throw new DivideByZeroException();
+                // throw new Exception("Мій перший Exception");
+                // throw new ArgumentException("My text!");
+                // Console.WriteLine($"Результат: {y}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"5: Ми згенерували виняток {ex.Message} StackTrace = {ex.StackTrace}"); 
+            }
+
+
+            // 6 Catch фільтри винятків
+            int x1 = 1;
+            int y1 = 0;
+
+            try
+            {
+                int result = x1 / y1;
+            }
+            catch (DivideByZeroException) when (y1 == 0)
+            {
+                Console.WriteLine("6: y не повинен дорівнювати 0");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"6: Ми згенерували виняток {ex.Message}");
+            }
+
+            // 7 Використання класу Exception
+            try
+            {
+                int x = 5;
+                int y = x / 0;
+                Console.WriteLine($"Результат: {y}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"7: Виключення: {ex.Message}");
+                Console.WriteLine($"7: Метод: {ex.TargetSite}");
+                Console.WriteLine($"7: Стек викликів: {ex.StackTrace}");
+            }
+
+            Console.Read();
+
         }
     }
 }
---
+++ Example_types/Example_Try_Catch_4_Stek/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Example_Try_Catch_4_Stek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.OutputEncoding = System.Text.Encoding.Unicode;
+            System.Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            try
+            {
+                TestClass.Method1();
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Catch в Main : {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally в Main");
+            }
+            Console.WriteLine("Кінець методу Main");
+            Console.Read();
+        }
+    }
+    class TestClass
+    {
+        public static void Method1()
+        {
+            try
+            {
+                Method2();
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine($"Catch в Method1 : {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally в Method1");
+            }
+            Console.WriteLine("Кінець методу Method1");
+        }
+        static void Method2()
+        {
+            try
+            {
+                int x = 8;
+                int y = x / 0;
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally в Method2");
+            }
+            Console.WriteLine("Кінець методу Method2");
+        }
+    }
+}

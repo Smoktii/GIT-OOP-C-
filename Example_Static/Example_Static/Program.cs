ea78b342eccd8a3e08725138723ba696762fa698
Smoktii <Smoktii@gmail.com>
2020-10-06 16:11:52 +0300

    С1

---
+++ Example_Static/Example_Static/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Example_Static
+{
+    class Account
+    {
+        public static decimal bonus = 100;
+        public decimal totalSum;
+        public Account(decimal sum)
+        {
+            totalSum = sum + bonus;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.OutputEncoding = System.Text.Encoding.Unicode;
+            System.Console.InputEncoding = System.Text.Encoding.Unicode;
+            
+            Console.WriteLine(Account.bonus);      // 100
+            Account.bonus += 200;                  // 300 
+
+            Account account1 = new Account(150);
+            Console.WriteLine($"account1.totalSum = {account1.totalSum}");   // 450
+
+            Account account2 = new Account(1000);
+            Console.WriteLine($"account1.totalSum = {account2.totalSum}");   // 1300
+
+            Console.ReadKey();
+        }
+    }
+ }
---
+++ Example_Static/Example_Static_Methods/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Example_Static_Methods
+{
+    class Account
+    {
+        private static decimal minSum = 100; // мінімальна допустима сума для всіх рахунків
+
+        public Account(decimal sum, decimal rate)
+        {
+            if (sum < MinSum) throw new Exception("Неприпустима сума!");
+            Sum = sum; 
+            Rate = rate;
+        }
+        public static decimal MinSum
+        {
+            get { return minSum; }
+            set { if (value > 0) minSum = value; }
+        }
+
+        public decimal Sum { get; private set; }    // сумма на счете
+        public decimal Rate { get; private set; }   // процентная ставка
+
+        // подсчет суммы на счете через определенный период по определенной ставке
+        public static decimal GetSum(decimal sum, decimal rate, int period)
+        {
+            decimal result = sum;
+            for (int i = 1; i <= period; i++)
+                result = result + result * rate / 100;
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

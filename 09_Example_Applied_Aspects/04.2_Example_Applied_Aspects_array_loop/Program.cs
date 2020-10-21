de86b03194080bbe00b3f32b4d58a6b9f5e892cd
Smoktii <Smoktii@gmail.com>
2020-10-22 00:46:58 +0300

    c1

---
+++ 09_Example_Applied_Aspects/04.2_Example_Applied_Aspects_array_loop/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _04._2_Example_Applied_Aspects_array_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.OutputEncoding = System.Text.Encoding.Unicode;
+            System.Console.InputEncoding = System.Text.Encoding.Unicode;
+            
+            int[] numbers = new int[] { 10, 20, 30, 40, 50 };
+            int count = 0;
+            Console.WriteLine($"Цікл foreach");
+            foreach (int i in numbers)
+            {
+                count++;
+                Console.WriteLine($"count = {count}, i = {i}");
+                // Неможливо змінити i
+                // i = i * i;
+            }
+
+            Console.WriteLine($"Цікл for");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"i = {i}, numbers[i] = {numbers[i]}");
+                numbers[i] = numbers[i] * numbers[i];
+                Console.WriteLine($"i = {i}, numbers[i] (x^2) = {numbers[i]}");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 09_Example_Applied_Aspects/08_Example_Applied_Aspects_Indexers_overload/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace _08_Example_Applied_Aspects_Indexers_overload
+{
+    class Person
+    {
+        // Визначення двох автовластивостей
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    class People
+    {
+        // Визначення множини співробітників
+        Person[] data;
+        public People()
+        {
+            data = new Person[5];
+        }
+        public Person this[int index]
+        {
+            get
+            {
+                return data[index];
+            }
+            set
+            {
+                data[index] = value;
+            }
+        }
+        public Person this[string name]
+        {
+            get
+            {
+                Person person = null;
+                foreach (var p in data)
+                {
+                    if (p?.Name == name)
+                    {
+                        person = p;
+                        break;
+                    }
+                }
+                return person;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.OutputEncoding = System.Text.Encoding.Unicode;
+            System.Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            People people = new People();
+            // Для ініціалізації об'єктів класів застосовується ініціалізатор
+            people[0] = new Person { Name = "Tom" };
+            people[1] = new Person { Name = "Bob" };
+
+            Console.WriteLine($"people[0].Name = {people[0].Name}"); // Tom
+            // Чому стоять одинарні лапки? 'Bob'
+            Console.WriteLine($"people['Bob'].Name = {people["Bob"].Name}"); // Bob 
+ 
+            Console.ReadKey();
+        }
+    }
+}
